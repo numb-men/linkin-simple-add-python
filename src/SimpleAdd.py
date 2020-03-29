eedc10b3032502c211027e453a610de9a4318763
le <41882416+numb-men@users.noreply.github.com>
2020-03-29 14:51:50 +0800

    Update SimpleAdd.py

--- src/SimpleAdd.py
+++ src/SimpleAdd.py
@@ -10,11 +10,10 @@ if __name__ == "__main__":
             output = sys.argv[i + 1]
     sums = ''
     with open(input) as fin:
-        lines = fin.readlines(1024)
-        for line in lines:
+        for line in fin.readlines():
             nums = line.split('\n')[0].split('+')
             sum = 0
-            for num in nums:
+            for num in nums:    
                 sum += int(num)
             sums += str(sum) + '\n'
     with open(output, mode='w') as fout:
